1fba934b2ffaf0dcbe353a25bfa763886f897acd
AnikDasPartha58 <Anikdaspartha58@gmail.com>
2022-09-22 00:17:30 +0600

    finish start design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Board from "./components/board/Board";
 import Start from "./components/start/Start";
-import './index.css'
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-        <div className="container">
-        <Start/>
-        <Board/>
-        </div>  
+      <div className="container">
+        {/* <Start/> */}
+        <Board />
+      </div>
     </div>
   );
 }
--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import Oicon from "../icons/Oicon";
+import Xicon from "../icons/Xicon";
 
 const Start = () => {
   return (
-    <div>
-      Start
+    <div className="start">
+      <div className="start__header">
+        <Xicon />
+        <Oicon />
+      </div>
+      <div className="card shadow-gray">
+        <h1 className="text-lg">Pick player 1'st mark</h1>
+        <div className="start__players">
+          <span className="start__players--active">
+            <Xicon color="dark" />
+          </span>
+          <span>
+            <Oicon color="light" />
+          </span>
+        </div>
+        <div>
+          <p className="text-light">remember: x goes first</p>
+        </div>
+      </div>
+      <div className="start__btns">
+        <button className="btn btn-yelow">new game (vs CPU)</button>
+        <button className="btn btn-blue">new game (vs Player)</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Start
+export default Start;
